7be80d2875eda1fc0a0bb8280a7083239a639cef
Hammad <hammadu696@gmail.com>
2023-12-09 20:26:07 +0500

    remove utem from cart functionaltity

--- hamx/core/models.py
+++ hamx/core/models.py
@@ -22,14 +22,17 @@ class Item(models.Model):
 
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     item =models.ForeignKey(Item, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
+    ordered =  models.BooleanField(default=False)
+    quantity = models.IntegerField(default=2)
+    
 
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     items= models.ManyToManyField(OrderItem)
-    start_date= models.DateTimeField(auto_now_add=True)
+    start_date= models.DateTimeField(auto_now_add=True) 
     ordered_date=models.DateTimeField()
     ordered =  models.BooleanField(default=False)
 
--- hamx/core/views.py
+++ hamx/core/views.py
@@ -23,7 +23,7 @@ class  ItemDetailView (DetailView):
 
 def add_to_cart(request, slug):
     item = get_object_or_404(Item,slug=slug)
-    order_item =OrderItem.objects.create(item = item)
+    order_item , created =OrderItem.objects.get_or_create(item = item, user = request.user,ordered =False)
     order_qs= Order.objects.filter(user= request.user, ordered =False)
     print("enter in function")
     if order_qs.exists() :
@@ -31,7 +31,29 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity +=1
             order_item.save()
-            print("enter if data is saved")
+        else:
+            order.items.add(order_item)
+    else:
+        ordered_date = timezone.now()
+        print("enter else")
+        order = Order.objects.create(user =request.user,ordered_date=ordered_date)
+        order.items.add(order_item)
+    return redirect("core:product",slug =slug) 
+
+
+def remove_from_cart(request,slug):
+    item = get_object_or_404(Item,slug=slug)
+    order_qs= Order.objects.filter(user= request.user, ordered =False)
+   
+    if order_qs.exists() :
+        order =order_qs[0]
+        #check if the item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(item=item,user= request.user,ordered =False)[0]
+            order.items.remove(order_item)
+        else:
+            # add msg syaing item does not in order 
+            return redirect("core:product",slug =slug) 
     else:
         ordered_date = timezone.now()
         print("enter else")
