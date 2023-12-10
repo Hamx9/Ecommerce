6c4be9816ba68088d5f1db4555e31c7df6911cb6
Hammad <hammadu696@gmail.com>
2023-12-10 21:23:34 +0500

    adding msges

--- hamx/core/models.py
+++ hamx/core/models.py
@@ -16,8 +16,11 @@ class Item(models.Model):
         return reverse("core:product", kwargs={"slug": self.slug})
     
     def get_add_to_cart_url(self):
-        print('enter in add to cart')
         return reverse("core:add-to-cart", kwargs={"slug": self.slug})
+    
+    def get_remove_from_cart_url(self):
+        print("enter in REMOVE")
+        return reverse("core:remove-from-cart", kwargs={"slug": self.slug})
     
 
 
--- hamx/core/views.py
+++ hamx/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .models import Item, Order,OrderItem
 from django.utils import timezone
 from django.views.generic import ListView, DetailView
+from django.contrib import messages
 # Create your views here.
 # def item_list(request):
 #     context = {
@@ -44,21 +45,22 @@ def add_to_cart(request, slug):
 def remove_from_cart(request,slug):
     item = get_object_or_404(Item,slug=slug)
     order_qs= Order.objects.filter(user= request.user, ordered =False)
-   
+    print(order_qs)
     if order_qs.exists() :
         order =order_qs[0]
         #check if the item is in the order
         if order.items.filter(item__slug=item.slug).exists():
             order_item = OrderItem.objects.filter(item=item,user= request.user,ordered =False)[0]
             order.items.remove(order_item)
+            print("enter in REMOVE fun")
         else:
             # add msg syaing item does not in order 
+            messages.info(request,"this item was added to you cart")
+            print("item does not exist")
             return redirect("core:product",slug =slug) 
     else:
-        ordered_date = timezone.now()
-        print("enter else")
-        order = Order.objects.create(user =request.user,ordered_date=ordered_date)
-        order.items.add(order_item)
+        return redirect("core:product",slug =slug) 
+       
     return redirect("core:product",slug =slug) 
 
 
