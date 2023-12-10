058e21ff6b945801e5935d22991f2e33196cf29c
Hammad <hammadu696@gmail.com>
2023-12-10 22:06:32 +0500

    ...

--- hamx/core/views.py
+++ hamx/core/views.py
@@ -26,19 +26,22 @@ def add_to_cart(request, slug):
     item = get_object_or_404(Item,slug=slug)
     order_item , created =OrderItem.objects.get_or_create(item = item, user = request.user,ordered =False)
     order_qs= Order.objects.filter(user= request.user, ordered =False)
-    print("enter in function")
+   
     if order_qs.exists() :
         order =order_qs[0]
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity +=1
             order_item.save()
+            messages.info(request,"this item was quantity updated")
         else:
             order.items.add(order_item)
+            messages.info(request,"this item was added to you cart")
     else:
         ordered_date = timezone.now()
-        print("enter else")
+       
         order = Order.objects.create(user =request.user,ordered_date=ordered_date)
         order.items.add(order_item)
+        messages.info(request,"this item was added to cart")
     return redirect("core:product",slug =slug) 
 
 
@@ -52,17 +55,15 @@ def remove_from_cart(request,slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item = OrderItem.objects.filter(item=item,user= request.user,ordered =False)[0]
             order.items.remove(order_item)
-            print("enter in REMOVE fun")
+            messages.info(request,"this item is removed from cart")
         else:
             # add msg syaing item does not in order 
-            messages.info(request,"this item was added to you cart")
-            print("item does not exist")
+            messages.info(request,"item does not exist in you cart")
             return redirect("core:product",slug =slug) 
     else:
+        messages.info(request,"you do not have a active order")
         return redirect("core:product",slug =slug) 
-       
-    return redirect("core:product",slug =slug) 
-
+        
 
 
 
