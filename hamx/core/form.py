bc5911ff34ec2e0afb88cfd0e02ca4ced15fba9e
hammad ullah <hammadu696@gmail.com>
2023-12-20 06:10:54 +0000

    check

--- hamx/core/form.py
+++ hamx/core/form.py
@@ -1,12 +1,17 @@
 from django import forms
 from django_countries.fields import CountryField
 
+PAYMENT_CHOICES=(
+('S','Stripe'),
+('P','Paypal')
+)
+
 class CheckOut(forms.Form):
-    street_adress = forms.CharField()
-    apartment_adress = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country)')
+    street_adress = forms.CharField(widget=forms.TextInput(attrs={ 'class':'form-control'}))
+    apartment_adress = forms.CharField(required=False,widget=forms.TextInput(attrs={ 'class':'form-control'}))
+    country = CountryField().formfield()
     zip = forms.CharField()
     same_billing_adress= forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
+    payment_option = forms.ChoiceField(widget=forms.RadioSelect(),choices=PAYMENT_CHOICES)
     
--- hamx/hamx/settings.py
+++ hamx/hamx/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'django_countries',
+    'django_countries',   
     'core'
 ]
 
